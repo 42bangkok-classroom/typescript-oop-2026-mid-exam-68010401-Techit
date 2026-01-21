7d731fc4684a6d0ee2e8fec88a6d4517cf3aee45
techit saenkham <68010401@kmitl.ac.th>
2026-01-21 15:36:30 +0700

    5

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,15 @@
-// Write your code below
+const score = Number(process.argv[2]);
+
+if (score >= 80) {
+  console.log("Grade is A")
+} else if (score >= 70) {
+  console.log("Grade is B")
+} else if (score >= 60) {
+  console.log("Grade is C")
+} else if(score >= 50){
+  console.log("Grade is D")
+}else if (score >= 0){
+  console.log("Grade is F")
+}else{
+    console.log("Invalid input")
+}
